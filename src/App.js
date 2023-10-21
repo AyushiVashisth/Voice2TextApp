099e2444cb028f4e342f06da3da58b918826deac
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-10-21 12:52:49 +0530

    add backend api and fix some bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SpeechRecognition, {
   useSpeechRecognition
 } from "react-speech-recognition";
@@ -10,23 +10,23 @@ import { BsFillMicFill } from "react-icons/bs";
 const App = () => {
   // Define the form fields
   const fields = [
-    "First Name",
-    "Last Name",
-    "State",
-    "District",
-    "Village",
-    "PAN Number",
-    "Aadhaar Number"
+    "firstName",
+    "lastName",
+    "state",
+    "district",
+    "village",
+    "panNumber",
+    "aadhaarNumber"
   ];
 
   // State variables to manage form data and recording
   const [transcribedText, setTranscribedText] = useState({});
   const [activeField, setActiveField] = useState(""); // Tracks the field currently being recorded
   const [recordingTranscript, setRecordingTranscript] = useState(""); // Stores the live recording transcript
+  const [isSubmitDisabled, setSubmitDisabled] = useState(true); // Track the submit button's disabled state
+
   const { transcript, resetTranscript, finalTranscript, listening } =
     useSpeechRecognition(); // Hook for speech recognition
-
-  console.log("recordingTranscript", recordingTranscript);
 
   // Function to toggle recording for a specific field
   const handleToggleRecording = (field) => {
@@ -56,7 +56,7 @@ const App = () => {
     if (numericValue.length <= 12) {
       setTranscribedText((prev) => ({
         ...prev,
-        "Aadhaar Number": numericValue
+        aadhaarNumber: numericValue
       }));
     }
   };
@@ -72,7 +72,7 @@ const App = () => {
       (field) => !filledFields.includes(field)
     );
 
-    const aadharNumber = transcribedText["Aadhaar Number"];
+    const aadharNumber = transcribedText["aadhaarNumber"];
     if (unfilledFields.length === 0) {
       if (aadharNumber.length !== 12) {
         // Display an error if Aadhar Number is not exactly 12 digits
@@ -81,10 +81,16 @@ const App = () => {
         // If all fields are filled and Aadhar Number is valid, submit the form
         const formData = { ...transcribedText };
         axios
-          .post("https://mockapi-hhv3.onrender.com/onefinitylabsApi", formData)
+          .post(
+            "https://onefinitylabsapi.onrender.com/onefinitylabsApi",
+            formData
+          )
           .then((response) => {
             console.log("Form data submitted successfully:", response.data);
             toast.success("Form data submitted successfully");
+            // Reset the form after submission
+            setTranscribedText({});
+            setSubmitDisabled(true);
           })
           .catch((error) => {
             console.error("Error submitting form data:", error);
@@ -99,31 +105,42 @@ const App = () => {
       );
     }
   };
+
+  // Enable/disable the "Submit" button based on form completion
+  useEffect(() => {
+    const filledFields = Object.values(transcribedText).filter(Boolean);
+    const isAadharValid =
+      transcribedText["aadhaarNumber"] &&
+      transcribedText["aadhaarNumber"].length === 12;
+
+    setSubmitDisabled(filledFields.length !== fields.length || !isAadharValid);
+  }, [transcribedText]);
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-pink-500 to-yellow-500 flex flex-col justify-center items-center">
-      <div className="border border-gray-300 rounded-l-md shadow-md p-6">
+      <div className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 border border-gray-300 rounded-l-md shadow-md p-6">
         <h1 className="text-3xl font-bold mb-6 text-center">Form</h1>
-        <div className="grid grid-cols-2 gap-4 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
           {fields.map((field, index) => (
             <div key={index} className="flex items-center">
-              <div className="flex flex-col">
+              <div className="flex flex-col w-full">
                 <div className="flex justify-between items-center mt-2">
                   <label className="text-black font-semibold">
                     {field}
                     <span className="text-red-900">*</span>
                   </label>
-                  <h4 className="text-gray-100">
+                  <h4 className="text-gray-200">
                     {activeField === field && listening
                       ? "   recording..."
                       : ""}
                   </h4>
                 </div>
                 <input
-                  type={field === "Aadhaar Number" ? "number" : "text"}
+                  type={field === "aadhaarNumber" ? "number" : "text"}
                   placeholder={field}
                   value={transcribedText[field] || ""}
                   onChange={(e) => {
-                    if (field === "Aadhaar Number") {
+                    if (field === "aadhaarNumber") {
                       handleAadharNumberChange(e);
                     } else {
                       setTranscribedText((prev) => ({
@@ -132,7 +149,7 @@ const App = () => {
                       }));
                     }
                   }}
-                  className="border border-gray-300 rounded-l-md px-4 py-2 w-54 shadow-md"
+                  className="border border-gray-300 rounded-l-md px-4 py-2 w-full shadow-md"
                   required
                 />
               </div>
@@ -175,7 +192,12 @@ const App = () => {
         {/* Submit button */}
         <button
           onClick={handleSubmit}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 shadow-md w-full"
+          className={`${
+            isSubmitDisabled
+              ? "bg-gray-400 cursor-not-allowed"
+              : "bg-blue-500 hover:bg-blue-700"
+          } text-white font-bold py-2 px-4 rounded mt-4 shadow-md w-full`}
+          disabled={isSubmitDisabled}
         >
           Submit
         </button>
